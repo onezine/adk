36f8a14df40613a08c19428a7e6e5154ba11f8e4
williamkarminten <william.karminten@gmail.com>
2013-09-18 14:05:26 +0800

    Added version 1.0.3

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < Devise::SessionsController
   def create  
     warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")  
-    render :status => 200, :json => { :error => "Success", 'version' => '1.0.2' }  
+    render :status => 200, :json => { :error => "Success", 'version' => '1.0.3' }  
   end  
   def destroy  
     super
